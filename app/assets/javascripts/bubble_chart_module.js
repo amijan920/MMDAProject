27a6d0c758237b1eb843609b0eede0c5c1c4653b
Amiel Reveche <amielreveche@gmail.com>
2015-02-09 19:48:13 +0800

    add line view on click

--- app/assets/javascripts/bubble_chart_module.js
+++ app/assets/javascripts/bubble_chart_module.js
@@ -5,6 +5,11 @@
 var BubbleChart, 
 	__bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
+
+var root = typeof exports !== "undefined" && exports !== null ? exports : this;
+var c_view_type = 'all';
+var c_line = 2;
+
 BubbleChart = (function() {
 	function BubbleChart(data) {
 		this.charge = __bind(this.charge, this);
@@ -22,10 +27,16 @@ BubbleChart = (function() {
 		this.bound_height = this.height - this.height/4;
 		this.data = data;
 
+		// this.colors = ["#8dd3c7", "#ffffb3",
+		// 	"#bebada", "#fb8072", "#80b1d3",
+		// 	"#fdb462", "#b3de69", "#fccde5", "#d9d9d9"];
+
+		this.colors = ["#e25b83", "#ec7735", "#70c49b", "#f6c14f", 
+		 		"#8d5641", "#2e91cb", "#53c4c8", "#b193c0", "#c6c6c0"]
+		
 		this.fill_color = d3.scale.ordinal()
 			.domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-			.range(["#e25b83", "#ec7735", "#70c49b", "#f6c14f", 
-				"#8d5641", "#2e91cb", "#53c4c8", "#b193c0", "#c6c6c0"]);
+			.range(this.colors);
 
 		this.line_centers = {};
 		for(var i = 0; i < data.length; i++) {
@@ -33,13 +44,20 @@ BubbleChart = (function() {
 			this.line_centers[data[i].lineID].x = this.width/8 + (data[i].lineID % 5) * (this.bound_width/5) + this.bound_width/10;
 	 		this.line_centers[data[i].lineID].y = this.height/8 + Math.floor(data[i].lineID / 5) * (this.bound_height/2) + this.bound_height/4;
 		}
-		console.log(this.line_centers);
 
+		this.station_count = {};
 
 		this.center = {
 			x: this.width/2,
 			y: this.height/2
 		};
+
+		this.line_outside = {};
+		for(var i = 0; i < data.length; i++) {
+			this.line_outside[data[i].lineID] = {};
+			this.line_outside[data[i].lineID].x = (((this.center.x) + Math.cos(data[i].lineID*Math.PI*2/9 + Math.PI)*this.width));
+	 		this.line_outside[data[i].lineID].y = (((this.center.y) + Math.sin(data[i].lineID*Math.PI*2/9 + Math.PI)*this.width));
+		}
 
 		this.damper = 0.1;
 		this.force = null;
@@ -52,8 +70,6 @@ BubbleChart = (function() {
 		this.create_edges();
 		this.create_vis();
 	}
-	// ((d.lineID % 5) * (_this.width/5) + _this.width/10 + ((Math.random()*20) -10)
-	// ((d.lineID / 5) * (_this.height/2) + _this.height/4 + ((Math.random()*20) - 10))
 
 	BubbleChart.prototype.create_nodes = function() {
 		this.data.forEach((function(_this) {
@@ -69,6 +85,9 @@ BubbleChart = (function() {
 					radius: ((Math.random() * 20) + 5),
 					value: 10
 				}
+				if(!_this.station_count[d.lineID])
+					_this.station_count[d.lineID] = 0;
+				_this.station_count[d.lineID]++;
 				return _this.nodes.push(node);
 			};
 		})(this));
@@ -91,13 +110,6 @@ BubbleChart = (function() {
 			}
 			this.edges.push(edge);
 		}
-
-		// this.data.forEach((function(_this) {
-		// 	return function(d) {
-
-		// 		return _this.edges.push(edge);
-		// 	}
-		// })(this));
 	}
 
 	BubbleChart.prototype.create_vis = function() {
@@ -143,7 +155,7 @@ BubbleChart = (function() {
 				}
 			})(this))
 			.attr("class", function(d) {
-				return "edge-line-" + d.line_id;
+				return "edge-line edge-line-" + d.line_id;
 			});
 
 		this.circles = this.vis.selectAll("circle")
@@ -170,8 +182,14 @@ BubbleChart = (function() {
 				return "bubble-" + d.station_id;
 			}).on("mouseover", function(d, i) {
 				$("#testp").text(d.station_name);
+				$(this).attr("stroke-width", 4);
 			}).on("mouseout", function(d, i) {
-				$("#testp").text("None");
+				$("#testp").text("");
+				$(this).attr("stroke-width", 1.5);
+			}).on("click", function(d) {
+				var c = {};
+				c.lineID = d.line_id;
+				zoom_line(c);
 			})
 			.attr("cx", function(d) {
 				return d.x;
@@ -180,7 +198,7 @@ BubbleChart = (function() {
 				return d.y;
 			})
 			.attr("class", function(d) {
-				return "circle-line-" + d.line_id;
+				return "circle-line circle-line-" + d.line_id;
 			});
 
 		return this.circles
@@ -291,11 +309,68 @@ BubbleChart = (function() {
 		})(this);
 	};
 
+	BubbleChart.prototype.display_by_rect = function(line_id) {
+		this.force.gravity(this.layout_gravity)
+			.charge(this.charge)
+			.friction(0.9)
+			.on("tick", (function(_this) {
+				return function(e) {
+					_this.circles.each(_this.move_towards_rect(e.alpha, line_id))
+						.attr("cx", function(d) { return d.x; })
+						.attr("cy", function(d) { return d.y; });
+					_this.lines.each(function(d){})
+						.attr("x1", (function(__this) {
+							return function(d) {
+								return __this.nodes[d.p1].x
+							}
+						})(_this))
+						.attr("x2", (function(__this) {
+							return function(d) {
+								return __this.nodes[d.p2].x
+							}
+						})(_this))
+						.attr("y1", (function(__this) {
+							return function(d) {
+								return __this.nodes[d.p1].y
+							}
+						})(_this))
+						.attr("y2", (function(__this) {
+							return function(d) {
+								return __this.nodes[d.p2].y
+							}
+						})(_this));
+				}
+			})(this));
+			this.force.start();
+	}
+
+	BubbleChart.prototype.move_towards_rect = function(alpha, line_id) {
+		return (function(_this) {
+			return function(d) {
+				//TODO stations
+				var stations = _this.station_count[d.line_id];
+				var rot = (Math.PI*2/stations)*d.station_id + Math.PI;
+				var target = {};
+
+				if(d.line_id == line_id) {
+					target.x = Math.min(_this.center.x + _this.width/3, Math.cos(rot)*300 + _this.center.x);
+					target.x = Math.max(_this.center.x - _this.width/3, target.x);
+					target.y = Math.min(_this.center.y + _this.height/3, Math.sin(rot)*300 + _this.center.y);
+					target.y = Math.max(_this.center.y - _this.height/3, target.y);
+				}
+				else {
+					target = _this.line_outside[d.line_id];
+				}
+
+				d.x = d.x + (target.x - d.x) * (_this.damper + 0.02) * alpha * 1.1;
+				d.y = d.y + (target.y - d.y) * (_this.damper + 0.02) * alpha * 1.1;
+			};
+		})(this);
+	};
+
 	return BubbleChart;
 })();
 
-var root;
-root = typeof exports !== "undefined" && exports !== null ? exports : this;
 
 function createChart() {
 	var chart, render_vis;
@@ -318,10 +393,20 @@ function createChart() {
 		};
 	})(this);
 
+	root.display_rect = (function(_this) {
+		return function(line_id) {
+			return chart.display_by_rect(line_id);
+		};
+	})(this);
+
 	root.toggle_view = (function(_this) {
-		return function(view_type) {
+		return function(view_type, line_id) {
+			c_view_type = view_type;
 			if(view_type === 'line') {
 				root.display_line();
+			}
+			else if(view_type === 'ind-line') {
+				root.display_rect(line_id);
 			}
 			else {
 				root.display_all();
@@ -330,15 +415,21 @@ function createChart() {
 	})(this);
 
 
+	// $("#view-scheme").click(function(e) {
+	// 	root.toggle_view('ind-line', c_line);
+	// 	$(".vis-btn").toggleClass("active", false);
+	// 	$("#view-scheme").toggleClass("active", true);
+	// });
+
 	$("#view-by-lines").click(function(e) {
-		root.display_line();
+		root.toggle_view('line');
+		$(".vis-btn").toggleClass("active", false);
 		$("#view-by-lines").toggleClass("active", true);
-		$("#view-all").toggleClass("active", false);
 	});
 
 	$("#view-all").click(function(e) {
-		root.display_all();
-		$("#view-by-lines").toggleClass("active", false);
+		root.toggle_view('all');
+		$(".vis-btn").toggleClass("active", false);
 		$("#view-all").toggleClass("active", true);
 	});
 
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,5 +1,6 @@
 function init_lineFilter(data) {
 	var $line_filter = d3.select("#line-filter");
+	var $drop_down = d3.select("#view-scheme-menu");
 
 	var i = 1;
 	var $line_divs = $line_filter.selectAll("div")
@@ -35,6 +36,45 @@ function init_lineFilter(data) {
 		.text("Show/Hide all");
 
 	$line_filter.select("#line-item-a").on("click", toggle_all);
+
+
+	var $drop_down_items = $drop_down.selectAll("li")
+		.data(data)
+		.enter()
+		.append("li")
+			.attr("role", "presentation");
+
+	$drop_down_items.append("div")
+		.attr('id', "line-square-a")
+		.attr('class', function(d) { return "line-square line-square-menu line-" + d.lineID + " "})
+		.attr('data-toggle', 1);
+
+	$drop_down_items.append("div")
+		.attr("data-line-id", function(d){ return d.lineID })
+		.attr("role", "menuitem")
+		.attr("tabindex", "-1")
+		.attr("class", "line-drop-down-item")
+		.text(function(d) { return d.lineName });
+
+	d3.selectAll('.line-drop-down-item').on('click', zoom_line);
+
+}
+
+function zoom_line(d) {
+	$(".vis-btn").toggleClass("active", false);
+	$("#view-scheme").toggleClass("active", true);
+	
+	var $line_square = $("#line-item-"+d.lineID).find(".line-square");
+	$line_square.data("toggled", 1);
+	$line_square.removeClass("unselected");
+	$(".circle-line-"+d.lineID).each ( function(e) {
+		$(this).fadeIn();
+	});
+	$(".edge-line-"+d.lineID).each ( function(e) {
+		$(this).fadeIn();
+	});
+
+	root.toggle_view('ind-line', d.lineID);
 }
 
 function toggle_lineFilter(d, i) {
@@ -62,13 +102,12 @@ function toggle_lineFilter(d, i) {
 }
 
 function toggle_all() {
-	console.log("clicked");
 	var $line_square = $("#line-item-a").find(".line-square");
 	if( $line_square.data("toggled") === 0) {
 		$line_square.data("toggled", 1);
 		$line_square.removeClass("unselected");
 
-		$(".line-square").each(function(e, i) {
+		$("#line-filter .line-square").each(function(e, i) {
 			id = $(this).data("id");
 			$(this).data("toggled", 1);
 			$(this).removeClass("unselected");
@@ -84,7 +123,7 @@ function toggle_all() {
 		$line_square.data("toggled", 0);
 		$line_square.addClass("unselected");
 
-		$(".line-square").each(function(e, i) {
+		$("#line-filter .line-square").each(function(e, i) {
 			id = $(this).data("id");
 			$(this).data("toggled", 0);
 			$(this).addClass("unselected");
